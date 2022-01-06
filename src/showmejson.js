d91b614f28d80a5ad8402507dbb64489ffaf1f1a
Roimer Garcia <rjgarcia@efactoryerp.com>
2022-01-05 23:54:13 -0400

    initial files and folders setup

---
+++ src/showmejson.js
@@ -0,0 +1,13 @@
+/** 
+ * @author Roimer Garcia
+ *      contact roimergarica@gmail.com
+ *      source available in https://github.com/roimergarcia/showmejson
+ *      date 2022-01-05
+ * @license  CC BY-SA 3.0 http://creativecommons.org/licenses/by-sa/3.0/deed.es
+ * 
+ * 
+*/
+
+const showmejson = { 'OK': true }
+
+export { showmejson };
