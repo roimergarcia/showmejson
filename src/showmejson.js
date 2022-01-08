27fbfa51ccb5dfe5ec70381a08f5a96ae5e92314
Roimer Garcia <roimergarcia@gmail.com>
2022-01-07 22:33:05 -0400

    first steps...

--- src/showmejson.js
+++ src/showmejson.js
@@ -1,13 +1,37 @@
 /** Showmejson
  * @author Roimer Garcia
- *      contact roimergarica@gmail.com
+ *      contact roimergarcia@gmail.com
  *      source available in https://github.com/roimergarcia/showmejson
  *      date 2022-01-05
  * @license  CC BY-SA 3.0 http://creativecommons.org/licenses/by-sa/3.0/deed.es
  * 
- * 
 */
 
-const showmejson = { 'OK': true }
+"use strict"
+
+
+const showmejson = { 
+    
+    /**
+     * Returns and HTML representarion of an object, in the form of a DocumentFragment.
+     * @param {Object} obj Object to be rendered as html
+     * @returns 
+     */
+    getFragment: function(obj){
+
+        const fragment = document.createDocumentFragment();
+
+        const part = document.createElement('div');
+        part.textContent = '' + obj;
+
+        fragment.append(part);
+        
+        return {
+            'OK': true, 
+            fragment
+        }
+
+    }
+};
 
 export { showmejson };
